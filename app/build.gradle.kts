fd0c2aec8d393fc7849d11817c8c1732cb34d0da
Luca <luca.monti@gmail.com>
2025-09-16 22:06:34 +0200

    V3.1 new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         //noinspection ExpiredTargetSdkVersion
         targetSdk = 27
         versionCode = 1
-        versionName = "3.0.1"
+        versionName = "3.1"
     }
 
     buildTypes {
